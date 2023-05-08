7abf53ea95eee0920baf83e0362468f14afcb902
Santiago-Acuna <santfloo7xcrl8@gmail.com>
2023-05-08 15:25:53 -0300

    env consoles borrados

--- src/assets/contact/contact.jsx
+++ src/assets/contact/contact.jsx
@@ -1,13 +1,10 @@
-import React, { useRef, useState } from 'react';
-import styles from './contact.module.css'
-import emailjs from '@emailjs/browser';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React, { useRef, useState } from "react";
+import styles from "./contact.module.css";
+import emailjs from "@emailjs/browser";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-export default function Contact({Language}){
-  console.log(import.meta.env.VITE_SERVICE) // 123
-  console.log(import.meta.env.VITE_TEMPLATE) // undefined
-  
+export default function Contact({ Language }) {
   const form = useRef();
   const [input, setInput] = useState({
     user_name: "",
@@ -23,7 +20,7 @@ export default function Contact({Language}){
     if (!input.user_name) {
       errors.user_name = "Username is required";
       setDisabled(true);
-    } 
+    }
 
     if (!input.user_email) {
       errors.user_email = "Email is required";
@@ -33,11 +30,11 @@ export default function Contact({Language}){
     if (!input.message) {
       errors.message = "Message is required";
       setDisabled(true);
-    } 
+    }
     return errors;
-  }
+  };
 
-  const handleChange =(e) => {
+  const handleChange = (e) => {
     setInput({
       ...input,
       [e.target.name]: e.target.value,
@@ -48,115 +45,122 @@ export default function Contact({Language}){
         [e.target.name]: e.target.value,
       })
     );
-  }
+  };
   const sendEmail = (e) => {
-    // import.meta.env.VITE_SERVICE import.meta.env.VITE_TEMPLATE import.meta.env.VITE_KEY
     e.preventDefault();
-    emailjs.sendForm(import.meta.env.VITE_SERVICE, import.meta.env.VITE_TEMPLATE, form.current,  import.meta.env.VITE_KEY)
-    // console.log("enviado")
-    // console.log(form)
+    emailjs
+      .sendForm(
+        import.meta.env.VITE_SERVICE,
+        import.meta.env.VITE_TEMPLATE,
+        form.current,
+        import.meta.env.VITE_KEY
+      )
 
-
-      .then((result) => {
+      .then(
+        (result) => {
           console.log(result.text);
-           console.log(form.current)
-      }, (error) => {
+          console.log(form.current);
+        },
+        (error) => {
           console.log(error.text);
-      });
-      e.target.reset();
+        }
+      );
+    e.target.reset();
   };
 
+  const notify = () => {
+    toast.success(" The message was sent!", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    });
+  };
 
-    const notify = () => {
-      toast.success(' The message was sent!', {
-        position: "top-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "colored",
-        });
-    }
-  
-
-
-        
-
-
-  return(
+  return (
     <div className={styles.box}>
       <section class="contact" id="contact">
-       {Language === "English" ? <div className={styles.divTitle}><p className={styles.title}>Contact</p></div>:
-        <div className={styles.divTitleE}><p className={styles.title}>Contacto</p></div>}
-      <p className={styles.p2}>{Language === "English" ? "// Submit the form below or shoot me an email" : "Envíame un mensaje o un correo electrónico"} - santiagoacu1990@gmail.com</p>
+        {Language === "English" ? (
+          <div className={styles.divTitle}>
+            <p className={styles.title}>Contact</p>
+          </div>
+        ) : (
+          <div className={styles.divTitleE}>
+            <p className={styles.title}>Contacto</p>
+          </div>
+        )}
+        <p className={styles.p2}>
+          {Language === "English"
+            ? "// Submit the form below or shoot me an email"
+            : "Envíame un mensaje o un correo electrónico"}{" "}
+          - santiagoacu1990@gmail.com
+        </p>
 
-
-    
-    <form ref={form} onSubmit={sendEmail} >
-    <div className={styles.divForm}>
-      <input
-        className={styles.inputBox}
-        type="text" 
-        name="user_name"
-        placeholder="Name"
-        autoComplete="off"
-        onChange={(e) => handleChange(e)}
-    />
-              {errors.user_name && (
-            <p className={styles.danger}>{errors.user_name}</p>
-          )}
-      <input
-        className={styles.inputBox}
-        type="email"
-        name="user_email" 
-        placeholder="Email"
-        autoComplete="off"
-        onChange={(e) => handleChange(e)}
-        
-    />
-    {errors.user_email && (
-            <p className={styles.danger}>{errors.user_email}</p>
-          )}
-      <textarea 
-  className={styles.textTareaBox}
-  maxLength="1000"
-  type="text" 
-  name="message"
-  placeholder="Message..."
-  onChange={(e) => handleChange(e)}
-
-  >
-  </textarea>
-  {errors.message && (
-            <p className={styles.danger}>{errors.message}</p>
-          )}
-  <div className={styles.btnDiv}>
-      <button disabled={disabled} onClick={notify} className={styles.btn} type="submit" value="Send">{Language === "English" ? "Send" : "Enviar"}</button>
-      
-      </div>
-      </div>
- </form>
- <ToastContainer
-position="top-right"
-autoClose={5000}
-hideProgressBar={false}
-newestOnTop={false}
-closeOnClick
-rtl={false}
-pauseOnFocusLoss
-draggable
-pauseOnHover
-theme="colored"
-/>
-
-
-
-    </section>
-
+        <form ref={form} onSubmit={sendEmail}>
+          <div className={styles.divForm}>
+            <input
+              className={styles.inputBox}
+              type="text"
+              name="user_name"
+              placeholder="Name"
+              autoComplete="off"
+              onChange={(e) => handleChange(e)}
+            />
+            {errors.user_name && (
+              <p className={styles.danger}>{errors.user_name}</p>
+            )}
+            <input
+              className={styles.inputBox}
+              type="email"
+              name="user_email"
+              placeholder="Email"
+              autoComplete="off"
+              onChange={(e) => handleChange(e)}
+            />
+            {errors.user_email && (
+              <p className={styles.danger}>{errors.user_email}</p>
+            )}
+            <textarea
+              className={styles.textTareaBox}
+              maxLength="1000"
+              type="text"
+              name="message"
+              placeholder="Message..."
+              onChange={(e) => handleChange(e)}
+            ></textarea>
+            {errors.message && (
+              <p className={styles.danger}>{errors.message}</p>
+            )}
+            <div className={styles.btnDiv}>
+              <button
+                disabled={disabled}
+                onClick={notify}
+                className={styles.btn}
+                type="submit"
+                value="Send"
+              >
+                {Language === "English" ? "Send" : "Enviar"}
+              </button>
+            </div>
+          </div>
+        </form>
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="colored"
+        />
+      </section>
     </div>
-  )
-
+  );
 }
-
