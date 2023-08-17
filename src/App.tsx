db60c5d66b6ce8f8b10551c73f9ba53380a23d19
Santiago Acuña <santfloo7xcrl8@gmail.com>
2023-08-17 16:19:25 -0300

    About added

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import styles from "./App.module.css";
+// import Navbar from "./Components/navbar/navbar";
+// import Presentation from "./Components/presentation/presentation";
+import About from "./Components/about/about.tsx";
+// import Skills from "./Components/skills/skills";
+// import Contact from "./Components/contact/contact";
+// import Footer from "./Components/footer/footer";
+// import LeftSideBar from "./Components/left sidebar/left sidebar";
+// import RightSideBar from "./Components/right sidebar/right sidebar";
+// import Projects from "./Components/projects/projects";
+// import Certificates from "./Components/certificates/certificates";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [Language, setLanguage] = useState<string>("English");
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className={styles.app}>
+      {/* <Navbar Language={Language} setLanguage={setLanguage} />
+      <div className={styles.mainContent}>
+        <Presentation className={styles.contentBox} Language={Language} /> */}
+        <About Language={Language} />
+        {/* <Skills Language={Language} />
+        <Certificates Language={Language} />
+        <Projects Language={Language} />
+        <Contact Language={Language} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <Footer Language={Language} />
+      <LeftSideBar Language={Language} />
+      <RightSideBar /> */}
+    </div>
+  );
 }
 
-export default App
+export default App;
