c4a9992561a369ef789c9e55b60e4be4502c048d
Santiago Acuña <santfloo7xcrl8@gmail.com>
2023-09-08 16:21:14 -0300

    solid priciples applied on project component

---
+++ src/Components/projects/projectsResume.ts
@@ -0,0 +1,5 @@
+export const recipesSumary: string =
+  "A SPA that consumes data from an external API, and allows general and detailed information of more than 5.000 recipes, among the functionalities it allows recipes and ordering recipes by different variables such as healthscore, diets, name.";
+
+export const hypeShopSumary: string =
+  " Development of an E-commerce that I carried out together with a team of developers. The application consists of the innate characteristics of an e-commerce (CRUD of products, authentication, catalogue, checkout, etc...). Payment gateway integration with Stripe. Sending transactional emails with NodeMailer. Management of shopping cart and product orders. User administration and password management. For project decision making, we opted for agile methodologies with weekly sprints (SCRUM).";
---
+++ src/Components/projects/projectsUtils.tsx
@@ -0,0 +1,85 @@
+import githubIcon from "../../Images/github.png";
+import githubIconRosa from "../../Images/github rosa.png";
+import redirectButtonWhite from "../../Images/redirect button white.png";
+import redirectButtonRose from "../../Images/redirect button rose.png";
+import styles from "./projects.module.css";
+import TranslateText from "../../assets/TranslateText/translateText.js";
+
+interface ImgLinkProps {
+  img1: string;
+  img2: string;
+  url: string;
+}
+
+interface ProjectsMold {
+  urlGithub: string;
+  urlDeploy: string;
+  imgPrime: string;
+  projectSumaryEnglish: string;
+  projectSumaryEspanish: string;
+}
+
+const ImgLink: React.FC<ImgLinkProps> = ({ img1, img2, url }) => {
+  return (
+    <a
+      className={styles.a1}
+      href={url}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img
+        className={styles.image}
+        src={img1}
+        onMouseOver={(e) => (e.currentTarget.src = `${img2}`)}
+        onMouseOut={(e) => (e.currentTarget.src = `${img1}`)}
+        alt="Link"
+      />
+    </a>
+  );
+};
+
+const ProjectsMold: React.FC<ProjectsMold> = ({
+  imgPrime,
+  urlGithub,
+  urlDeploy,
+  projectSumaryEnglish,
+  projectSumaryEspanish,
+}) => {
+  return (
+    <div>
+      <p className={styles.proyectTitle}>Recipes App</p>
+      <div className={styles.mainContent}>
+        <img
+          className={styles.img1}
+          src={imgPrime}
+          alt={`${projectSumaryEnglish} img`}
+        />
+        <div className={styles.description}>
+          <div className={styles.summary}>
+            <p className="">
+              <TranslateText
+                English={projectSumaryEnglish}
+                Spanish={projectSumaryEspanish}
+              />
+            </p>
+          </div>
+          <p className={styles.p2}>
+            React Redux Css Nodejs Express Sequelize.js PostgreSQL Spoonacular
+            API
+          </p>
+          <div></div>
+        </div>
+      </div>
+      <div className={styles.diva}>
+        <ImgLink img1={githubIcon} img2={githubIconRosa} url={urlGithub} />
+        <ImgLink
+          img1={redirectButtonWhite}
+          img2={redirectButtonRose}
+          url={urlDeploy}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ProjectsMold;
