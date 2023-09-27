32a7ac125ea3a0eb02bb8a471cf893aaa6833945
Santiago Acuña <santfloo7xcrl8@gmail.com>
2023-09-27 16:38:50 -0300

    css upgrade

--- src/Components/certificates/certificates.tsx
+++ src/Components/certificates/certificates.tsx
@@ -1,28 +1,18 @@
-import React, { useContext } from "react";
+import React from "react";
 import styles from "./certificates.module.css";
 import Carousel from "./carousel/carousel";
 import HenryCertificate from "../../Images/Henry certificate.png";
 import EFCertificate from "../../Images/EF certificate.png";
-import { GlobalLanguageState } from "../../assets/GlobalStates/globalStates.tsx";
+import Title from "../../assets/title/title.tsx";
 
 const Certificates: React.FC = () => {
-  const { Language } = useContext(GlobalLanguageState)!;
   const slides: string[] = [HenryCertificate, EFCertificate];
 
   return (
     <div className={styles.box}>
       <section id="Certificates">
-        <div>
-          {Language === "English" ? (
-            <div className={styles.divTitle}>
-              <p className={styles.title}>Certificates</p>
-            </div>
-          ) : (
-            <div className={styles.divTitleE}>
-              <p className={styles.title}>Certificados</p>
-            </div>
-          )}
-        </div>
+       
+      <Title English="Certificates" Spanish="Certificados" />
         <div className={styles.container}>
           <Carousel slides={slides} />
         </div>
