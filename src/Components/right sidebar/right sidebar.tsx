5930c77762aadd7c301b4a5c6ee9ef5692ecd7b2
Santiago Acuña <santfloo7xcrl8@gmail.com>
2023-11-30 16:25:07 -0300

    deleted unecesary code

--- src/Components/right sidebar/right sidebar.tsx
+++ src/Components/right sidebar/right sidebar.tsx
@@ -14,20 +14,3 @@ const RightSideBar: React.FC = () => {
 };
 
  export default RightSideBar;
-
-
-// import React from 'react';
-// import styles from './right sidebar.module.css'; // Import your component-specific styles
-
-// const RightSideBar: React.FC = () => {
-//   return (
-//     <div className={styles.rightContainer}>
-//       <div className={styles.textContainer}>
-//         <p className={styles.centerRightText}>santiagoacu1990@gmail.com</p>
-//       </div>
-//       <div className={styles.verticalLine}></div>
-//     </div>
-//   );
-// };
-
-// export default RightSideBar;
