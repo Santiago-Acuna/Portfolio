3a888c0ddbf2db5562b38886a211cc56ee8bd7c2
Santiago Acuña <santfloo7xcrl8@gmail.com>
2023-11-30 16:24:05 -0300

    right side bar component added css reponsive settings

--- src/Components/right sidebar/right sidebar.tsx
+++ src/Components/right sidebar/right sidebar.tsx
@@ -3,15 +3,31 @@ import styles from './right sidebar.module.css';
 
 const RightSideBar: React.FC = () => {
   return (
-    <div>
-      <div className={styles.box}>
+        <div className={styles.box}>
         <div className={styles.p1Box}>
-          <p className={styles.p1}>santiagoacu1990@gmail.com</p>
+          <span className={styles.p1}>santiagoacu1990@gmail.com</span>
         </div>
         <div className={styles.verticalBar}></div>
       </div>
-    </div>
+
   );
 };
 
-export default RightSideBar;
+ export default RightSideBar;
+
+
+// import React from 'react';
+// import styles from './right sidebar.module.css'; // Import your component-specific styles
+
+// const RightSideBar: React.FC = () => {
+//   return (
+//     <div className={styles.rightContainer}>
+//       <div className={styles.textContainer}>
+//         <p className={styles.centerRightText}>santiagoacu1990@gmail.com</p>
+//       </div>
+//       <div className={styles.verticalLine}></div>
+//     </div>
+//   );
+// };
+
+// export default RightSideBar;
